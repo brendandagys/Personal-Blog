9eaa36e01d624d05b1cff4a96e757ecd2e346539
Brendan Dagys <brendandagys@gmail.com>
2021-12-22 12:29:30 -0500

    Ready for deployment

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router'
+
 import { getPosts, getPostDetails } from '../../services'
 
 import {
@@ -7,9 +9,16 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from '../../components'
 
 const PostDetails = ({ post }) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loader />
+  }
+
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
@@ -48,6 +57,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   }
 }
