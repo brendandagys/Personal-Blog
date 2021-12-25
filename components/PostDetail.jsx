5abed1b087b518b0e655648e358ac649be659223
Brendan Dagys <brendandagys@gmail.com>
2021-12-25 17:47:09 -0500

    Fix RTF printing for <a> and code block elements
    Add AdjacentPosts feature

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,57 +1,63 @@
 import moment from 'moment'
 import getContentFragment from '../services/rtfUtility'
 
-const PostDetail = ({ post }) => (
-  <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
-    <div className='relative overflow-hidden shadow-md mb-6'>
-      <img
-        src={post.featuredImage.url}
-        alt={post.title}
-        className='object-top h-full w-full rounded-t-lg'
-      />
-    </div>
-    <div className='px-4 lg:px-0'>
-      <div className='flex items-center mb-8 w-full'>
-        <div className='flex items-center mb-4 lg:mb-0 lg:w-auto mr-8'>
-          <img
-            alt={post.author.name}
-            height='40px'
-            width='40px'
-            className='align-middle rounded-full'
-            src={post.author.photo.url}
-          />
-          <p className='inline align-middle text-gray-700 ml-3 text-lg'>
-            {post.author.name}
-          </p>
-        </div>
-        <div className='font-medium text-gray-700 ml-auto mr-6 mb-3.5'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6 inline mr-2 text-pink-500'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
-            />
-          </svg>
-          <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
-        </div>
-      </div>
-      <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
-      {post.content.raw.children.map((typeObj, index) => {
-        const children = typeObj.children.map((item, itemIndex) =>
-          getContentFragment(itemIndex, item.text, item)
-        )
+const PostDetail = ({ post }) => {
+  // console.log(post.content.raw.children)
+  // console.log(post.content.raw.children[1])
+  // console.log(post.content.raw.children[15])
 
-        return getContentFragment(index, children, typeObj, typeObj.type)
-      })}
+  return (
+    <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
+      <div className='relative overflow-hidden shadow-md mb-6'>
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className='object-top h-full w-full rounded-t-lg'
+        />
+      </div>
+      <div className='px-4 lg:px-0'>
+        <div className='flex items-center mb-8 w-full'>
+          <div className='flex items-center mb-4 lg:mb-0 lg:w-auto mr-8'>
+            <img
+              alt={post.author.name}
+              height='40px'
+              width='40px'
+              className='align-middle rounded-full'
+              src={post.author.photo.url}
+            />
+            <p className='inline align-middle text-gray-700 ml-3 text-lg'>
+              {post.author.name}
+            </p>
+          </div>
+          <div className='font-medium text-gray-700 ml-auto mr-6 mb-3.5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6 inline mr-2 text-pink-500'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
+              />
+            </svg>
+            <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+          </div>
+        </div>
+        <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((obj, itemIndex) =>
+            getContentFragment(itemIndex, obj.text, obj)
+          )
+
+          return getContentFragment(index, children, typeObj, typeObj.type)
+        })}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default PostDetail
--- services/rtfUtility.js
+++ services/rtfUtility.js
@@ -1,4 +1,6 @@
 import { Fragment } from 'react'
+import Highlight from 'react-highlight'
+import '../node_modules/highlight.js/styles/atom-one-dark.css'
 
 const getContentFragment = (index, text, obj, type) => {
   let modifiedText = text
@@ -15,6 +17,34 @@ const getContentFragment = (index, text, obj, type) => {
     if (obj.underline) {
       modifiedText = <u key={index}>{text}</u>
     }
+    if (obj.code) {
+      modifiedText = (
+        <code
+          style={{
+            backgroundColor: '#f8f9fb',
+            borderRadius: '3px',
+            border: '1px solid #dcdcdc',
+            padding: '0 3px',
+          }}
+          key={index}
+        >
+          {text}
+        </code>
+      )
+    }
+    if (obj.type === 'link') {
+      modifiedText = (
+        <a
+          key={index}
+          style={{ color: '#0000ee' }}
+          rel='noreferrer'
+          target='_blank'
+          href={obj.href}
+        >
+          {obj.children[0].text}
+        </a>
+      )
+    }
   }
 
   switch (type) {
@@ -25,6 +55,14 @@ const getContentFragment = (index, text, obj, type) => {
             <Fragment key={i}>{item}</Fragment>
           ))}
         </h3>
+      )
+    case 'heading-five':
+      return (
+        <h5 key={index} className='text-xl font-semibold mb-4'>
+          {modifiedText.map((item, i) => (
+            <Fragment key={i}>{item}</Fragment>
+          ))}
+        </h5>
       )
     case 'paragraph':
       return (
@@ -52,6 +90,18 @@ const getContentFragment = (index, text, obj, type) => {
           src={obj.src}
         />
       )
+    case 'code-block':
+      return (
+        <Fragment key={index}>
+          {modifiedText.map((item, i) => (
+            <Fragment key={i}>
+              <Highlight>{item}</Highlight>
+              <br />
+            </Fragment>
+          ))}
+        </Fragment>
+      )
+
     default:
       return modifiedText
   }
