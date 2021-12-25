076b60de7792b289a1d99d4967b01324471029c1
Brendan Dagys <brendandagys@gmail.com>
2021-12-24 21:39:17 -0500

    Add CORS to API and change development port to 3005

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -7,10 +7,34 @@
 
 import { GraphQLClient, gql } from 'graphql-request'
 
+import Cors from 'cors'
+
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 const graphCMSToken = process.env.GRAPHCMS_TOKEN
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 export default async function comments(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
   // res.status(200).json({ name: 'John Doe' })
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
--- pages/api/health.js
+++ pages/api/health.js
@@ -1,3 +1,27 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 export default async function health(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
   return res.status(200).send('API server for `my-blog` is healthy!')
 }
